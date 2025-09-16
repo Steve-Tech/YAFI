8fcb2098cbcbf481cfe67e9636f72d114d615e8c
Stephen Horvath <s.horvath@outlook.com.au>
2025-09-16 11:26:31 +1000

    Release 0.5

--- yafi/main.py
+++ yafi/main.py
@@ -126,7 +126,7 @@ class YafiApplication(Adw.Application):
             developers=["Stephen Horvath"],
             issue_url="https://github.com/Steve-Tech/YAFI/issues",
             license_type=Gtk.License.GPL_2_0,
-            version="0.4",
+            version="0.5",
             website="https://github.com/Steve-Tech/YAFI",
         )
         about.add_acknowledgement_section(None, ["Framework Computer Inc. https://frame.work/"])
